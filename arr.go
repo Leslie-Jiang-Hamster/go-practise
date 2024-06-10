d6dca50e28358a2fc449071cf9d5dc2238f3d73a
Leslie_Jiang <Leslie_Jiang@proton.me>
2024-06-10 21:21:19 +0800

    feat: arr

---
+++ arr.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{3: 3}
+	fmt.Println(a)
+}
---
+++ if.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 3 % 2 == 0 {
+		fmt.Println("3 is even")
+	} else {
+		fmt.Println("3 is odd")
+	}
+}
